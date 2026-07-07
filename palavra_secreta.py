c56896d583e975342055436fa8619d96a361016c
Thiago Oliveira <thiago.oliveiraso06@gmail.com>
2026-07-07 20:25:45 -0300

    criei a logica de acerto

--- palavra_secreta.py
+++ palavra_secreta.py
@@ -1,3 +1,18 @@
 #Jogo da palavra secreta#
+palavra_secreta = 'Flora'
+letras_acertadas = ''
+while True:
+    letra_digitada = input("Digite uma letra: ")
 
-print = input("Digite uma letra: ")
+    if len(letra_digitada) > 1:
+        print("Digite apenas 1 letra")
+        continue
+
+    if letra_digitada in palavra_secreta:
+        letras_acertadas += letra_digitada
+    
+    for letra_secreta in palavra_secreta:
+        if letra_secreta in letras_acertadas:
+            print(letra_secreta)
+        else:
+            print('*')
