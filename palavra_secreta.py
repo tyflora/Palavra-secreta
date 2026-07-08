0c49f8137254081a7b9603f8ad927c3edc5c08a8
Thiago Oliveira <thiago.oliveiraso06@gmail.com>
2026-07-07 21:26:42 -0300

    Adicionei contagem de tentativas

--- palavra_secreta.py
+++ palavra_secreta.py
@@ -1,18 +1,28 @@
 #Jogo da palavra secreta#
 palavra_secreta = 'Flora'
 letras_acertadas = ''
+numero_tentativas = 0
 while True:
     letra_digitada = input("Digite uma letra: ")
-
+    numero_tentativas += 1
     if len(letra_digitada) > 1:
         print("Digite apenas 1 letra")
         continue
 
     if letra_digitada in palavra_secreta:
         letras_acertadas += letra_digitada
-    
+       
+    palavra_formada =''
     for letra_secreta in palavra_secreta:
         if letra_secreta in letras_acertadas:
-            print(letra_secreta)
+          palavra_formada += letra_secreta
         else:
-            print('*')
+            palavra_formada += '*'
+
+
+    print('palavra formada:', palavra_formada)
+
+    if palavra_formada == palavra_secreta:
+            print("VOCE GANHOU!!")
+            print("A palavra era", palavra_formada)
+            print("Tentativas:", numero_tentativas)
